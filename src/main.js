17250266011a4a17fe58b3ca43e84c310bc8a854
Anastasiia Kononova <bjko.smsng@gmail.com>
2026-07-08 17:02:42 +0300

    modify presenters for dynamic templates, templates now get data from outside

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import {render} from './render.js';
+import DestinationsModel from './model/destinations-model.js';
+import PointsModel from './model/points-model.js';
+import OffersModel from './model/offers-model.js';
 import FilterView from './view/filter.js';
 import BoardPresenter from './presenter/board-presenter.js';
 
@@ -6,8 +9,15 @@ const filterContainer = document.querySelector('.trip-controls__filters');
 
 const mainElement = document.querySelector('.trip-events');
 
+const pointsModel = new PointsModel();
+const destinationsModel = new DestinationsModel();
+const offersModel = new OffersModel();
+
 const boardPresenter = new BoardPresenter({
   container: mainElement,
+  pointsModel,
+  destinationsModel,
+  offersModel
 });
 
 render(new FilterView(), filterContainer);
